74934e6c9c1245ab7f5ec94fc74f49d2f6ca1464
Sahil Patel <Sahilp.patel1300@gmail.com>
2025-03-08 17:20:49 -0500

    final structure setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useQuery } from "convex/react";
 import { Button } from "@/components/ui/button";
+import { api } from "../convex/_generated/api";
+import { useState } from "react";
 
 function App() {
+  const [showMessage, setShowMessage] = useState(false);
+  const message = useQuery(api.hello.greet, {
+    name: " world",
+  });
+
   return (
-    <div className="flex justify-center items-center text-3xl min-h-screen">
-      <Button>Hello World</Button>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button
+        variant={showMessage ? "outline" : "default"}
+        onClick={() => setShowMessage(!showMessage)}
+      >
+        {`Click to ${showMessage ? "hide" : "show"} a message from the backend`}
+      </Button>
+      {showMessage && (
+        <div className="m-2">
+          Backend says:{" "}
+          <code className="border px-2 py-1 rounded-md text-sm">{message}</code>
+        </div>
+      )}
     </div>
   );
 }
