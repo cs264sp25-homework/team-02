5efe956fe98b20746774d908f28b36fa7ddfc039
Miseok Kim <amcdrmkr@gmail.com>
2025-03-11 12:37:44 -0400

    switch convex project to shared project on team account

--- convex/hello.ts
+++ convex/hello.ts
@@ -1,11 +1,11 @@
 import { query } from "./_generated/server";
-import { v } from "convex/values"
+import { v } from "convex/values";
 
 export const greet = query({
   args: {
-    name: v.optional(v.string())
-  }, 
+    name: v.optional(v.string()),
+  },
   handler: async (ctx, args) => {
-    return `Hello, ${args.name}!`
-  }
-})
+    return `Hello, ${args.name}!`;
+  },
+});
