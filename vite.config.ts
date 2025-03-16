b5e437ecbc4db121f2f767006a6bc606044c7358
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2025-03-16 11:09:07 -0400

    configure base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: isProduction ? "/team-02/" : "/", // this needs to match the repo name to work on github pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
